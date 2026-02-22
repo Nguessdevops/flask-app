3b39883aff5ef5de9d163ef3227b3c3e4e32e619
Nguessdevops <sonohnguess@gmail.com>
2026-02-22 13:19:44 +0000

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,3 +4,19 @@ def test_hello():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
+    from app import app
+
+def test_hello_status():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_hello_contenu():
+    client = app.test_client()
+    response = client.get('/')
+    assert b"Mon premier serveur" in response.data
+
+def test_route_inexistante():
+    client = app.test_client()
+    response = client.get('/pagedoesnotexist')
+    assert response.status_code == 404
